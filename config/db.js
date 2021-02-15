05599a8c59ced38a9c89708f72d9f37fa10fdda5
ii02735 <yadallee.bilaal@outlook.fr>
2021-02-15 01:35:49 +0100

    [HEROKU] Use SSL for postgres if production

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 require("dotenv").config()
-const connectionString = process.env.DATABASE_URL    
+const connectionString = process.env.DATABASE_URL
 const { Pool } = require('pg')
-const pool = new Pool({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production'
-})
+const pool = process.env.NODE_ENV != "development" ? new Pool({ connectionString, ssl: true }) : new Pool({ connectionString })
 module.exports = {
   query: (text, params) => pool.query(text, params)
 }
